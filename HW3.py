270b3640aa474c2296e0c532ebddc5625be7cab7
MaxShulha94 <maxshulga94@gmail.com>
2023-07-22 20:10:39 +0300

    first commit

--- HW3.py
+++ HW3.py
@@ -24,5 +24,18 @@ print(result)
 my_list = list(input('Enter your list: '))
 last = my_list[-1]
 my_list.insert(0, last)
-
 print(my_list[0:len(my_list)-1:1])
+
+# Написати програму, яка переміщає всі нулі в кінці списку.
+# Ваша задача — змінити список так, щоб всі нулі опинились в кінці списку.
+# Порядок всіх інших чисел повинен зберегтися!
+
+list_with0 = list(input('Enter your list: '))
+count0 = list_with0.count('0')
+if '0' in list_with0:
+    new_list = list('0'.join(list_with0).replace('0', '') + ('0'* count0))
+print(new_list)
+
+
+
+
