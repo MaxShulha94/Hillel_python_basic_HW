385b208b4b8011dd420cbfbce38b893f718c74d9
MaxShulha94 <maxshulga94@gmail.com>
2023-08-08 22:54:05 +0300

    first commit

--- HW8.py
+++ HW8.py
@@ -22,7 +22,7 @@ example_func(100000)
 import functools
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 odd_numbers = [i for i in numbers if i % 2 != 0]
-sq_odd = map(lambda x: x ** 2, odd_numbers)
+sq_odd = map(lambda x: x ** 2, filter(lambda x: x % 2 != 0, numbers))
 sum_sq_odd = functools.reduce(lambda x, y: x + y, sq_odd)
 res = sum_sq_odd / len(odd_numbers)
 print(res)
