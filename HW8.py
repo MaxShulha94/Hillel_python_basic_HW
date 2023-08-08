68c74fdda3c7aefac563ad0538c0035a95894cd0
MaxShulha94 <maxshulga94@gmail.com>
2023-08-08 22:48:52 +0300

    first commit

--- HW8.py
+++ HW8.py
@@ -1,21 +1,21 @@
 # Написати декоратор, котрий задекорує функцію і порахує час виконання самої функції.
-# import time
-# def calc_time(func):
-#     def wrapper(*args):
-#         start_time = time.time()
-#         res = func(*args)
-#         finish_time = time.time()
-#         duration = finish_time - start_time
-#         print(duration)
-#         return res
-#     return wrapper
-#
-# @calc_time
-# def example_func(*args):
-#     for i in range(*args):
-#         print(i ** 2)
-#
-# example_func(100000)
+import time
+def calc_time(func):
+    def wrapper(*args):
+        start_time = time.time()
+        res = func(*args)
+        finish_time = time.time()
+        duration = finish_time - start_time
+        print(duration)
+        return res
+    return wrapper
+
+@calc_time
+def example_func(*args):
+    for i in range(*args):
+        print(i ** 2)
+
+example_func(100000)
 
 # Вам потрібно обчислити середнє значення квадратів непарних чисел зі списку. Використовуйте функції map, filter
 # та reduce.
